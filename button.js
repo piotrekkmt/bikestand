b10419e7e3bfa358c89cd91e0951415a887f6140
Peter Poliwoda <peterpoliwoda@ie.ibm.com>
2015-11-07 17:15:55 +0000

    Show lights

--- button.js
+++ button.js
@@ -1,5 +1,15 @@
 var GpioStream = require('gpio-stream'),
     button = GpioStream.readable(17);
- 
+    led = GpioStream.writable(18),
+    http = require('http');
+
 // pipe the button presses to stdout
-button.pipe(process.stdout);
+//button.pipe(process.stdout);
+var stream = button.pipe(led);
+stream.pipe(process.stdout);
+
+//http.createServer(function (req, res) {
+//  res.setHeader('Content-Type', 'text/html');
+//  res.write('<pre>logging button presses:\n');
+//  stream.pipe(res);
+//}).listen(8080);
