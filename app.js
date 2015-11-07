0df45edc6e097250b2a875a463fce8098e23caf4
Peter Poliwoda <peterpoliwoda@ie.ibm.com>
2015-11-07 17:25:30 +0000

    light show 2

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+// button is attaced to pin 17, led to 18
+var GPIO = require('onoff').Gpio,
+    led = new GPIO(18, 'out'),
+    button = new GPIO(17, 'in', 'both');
+
+//var GpioStream = require('gpio-stream'),
+//    button = GpioStream.readable(17);
+//    led = GpioStream.writable(18),
+        http = require('http');
+
 var Firebase = require("firebase");
 var colors = require('colors');
 
@@ -5,42 +15,39 @@ var myFirebaseRef = new Firebase("https://bikestand.firebaseio.com/");
 var bay1; 
 
 myFirebaseRef.on("value", function(snapshot) {
-    
-    console.log(snapshot.val());
-    bay1 = snapshot.val().bay1;
+  console.log(snapshot.val());
 });
 
-var bay1New;
 
-while(true){
-    if(bay1New !== bay1){
-        bay1 = bay1New;
-        myFirebaseRef.child('bay1').set(bay1New);
-    }
+console.log("Let the Light show begin!");
+
+//var stream = button.pipe(led);
+//stream.pipe(process.stdout);
+
+//http.createServer(function (req, res) {
+//  res.setHeader('Content-Type', 'text/html');
+//  res.write('<pre>logging button presses:\n');
+//  stream.pipe(res);
+//}).listen(8080);
+
+
+// define the callback function
+function light(err, state) {
+  
+  // check the state of the button
+  // 1 == pressed, 0 == not pressed
+  if(state == 1) {
+    // turn LED on
+    led.writeSync(1);
+    myFirebaseRef.child('bay1').set('true');
+  } else {
+    // turn LED off
+    led.writeSync(0);
+    myFirebaseRef.child('bay1').set('true');
+  }
+
 }
-
-
-
-//// button is attaced to pin 17, led to 18
-//var GPIO = require('onoff').Gpio,
-//    led = new GPIO(18, 'out'),
-//    button = new GPIO(17, 'in', 'both');
-// 
-//// define the callback function
-//function light(err, state) {
-//  
-//  // check the state of the button
-//  // 1 == pressed, 0 == not pressed
-//  if(state == 1) {
-//    // turn LED on
-//    led.writeSync(1);
-//  } else {
-//    // turn LED off
-//    led.writeSync(0);
-//  }
-//  
-//}
-// 
-//// pass the callback function to the
-//// as the first argument to watch()
-//button.watch(light);
+ 
+// pass the callback function to the
+// as the first argument to watch()
+button.watch(light);
